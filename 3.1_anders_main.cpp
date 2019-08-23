465ffd0ed4ef45b3900aecc1a3a4d2f837054294
andebraa <54434858+andebraa@users.noreply.github.com>
2019-08-23 10:50:42 +0200

    Add files via upload

---
+++ 3.1_anders_main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <new>
+
+using namespace std;
+
+float f(float x){
+    x = atan(x);
+    return x;
+}
+
+float f_deriv(float x, float h){
+    float h_inv = (1/h);
+    float deriv = (f(x+h) - f(x))*h_inv;
+
+    return deriv;
+}
+
+float f_deriv_alt(float x, float h)
+{
+    float h_inv = (1/(2*h));
+    float deriv = (f(x+h) - f(x-h))*h_inv;
+    return deriv;
+}
+
+int main()
+{
+    int stepnum = 10; //number of elements in stepsize
+    float x = sqrt(2);
+    double * stepsize;
+    stepsize = new*double[stepnum];
+
+
+
+
+    for ( float h=10; h>-100; h-- ){
+        printf(" %g : %g", h, f_deriv( x,  h));
+    }
+
+}
